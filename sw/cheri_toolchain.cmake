ba482c8f7f48698f57f4b589bea9e7982e59c586
Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>
2025-12-19 14:48:24 +0000

    Move reset vectors to `init_vectors.S`, simplify linkerscript
    Signed-off-by: Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>

--- sw/cheri_toolchain.cmake
+++ sw/cheri_toolchain.cmake
@@ -2,8 +2,8 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-set(CHERI_FLAGS -march=rv64ic_zcherihybrid -mabi=l64pc128 -cheri)
-set(NON_CHERI_FLAGS -march=rv64imc -mabi=lp64 -mcmodel=medany)
+set(CHERI_FLAGS -march=rv64imc_zcherihybrid -mabi=l64pc128)
+set(NON_CHERI_FLAGS -march=rv64imc -mabi=lp64 -mcmodel=medlow)
 
 set(CMAKE_SYSTEM_NAME Generic)
 
@@ -26,6 +26,12 @@ set(CMAKE_CXX_COMPILER_TARGET riscv64-unknown-elf)
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 
+# linkerscript to use
+set(LDS "${CMAKE_SOURCE_DIR}/device/boot/mocha.ld")
+
+# common objects to include in most executables
+set(COMMON_OBJS "${CMAKE_SOURCE_DIR}/device/boot/init_vectors.S")
+
 string(CONCAT CMAKE_CXX_FLAGS_INIT
   "-std=c++20 -O0 -g"
   " -fno-builtin -fno-exceptions -fno-c++-static-destructors -fno-rtti -Wall -Wextra"
@@ -38,4 +44,6 @@ string(CONCAT CMAKE_C_FLAGS_INIT
 
 set(CMAKE_ASM_FLAGS_INIT "")
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-nodefaultlibs -nostartfiles -Tlink.ld -L${CMAKE_CURRENT_LIST_DIR}" )
+set(CMAKE_EXE_LINKER_FLAGS_INIT
+    "-nodefaultlibs -nostartfiles -T ${LDS} -L ${CMAKE_CURRENT_LIST_DIR}"
+)
