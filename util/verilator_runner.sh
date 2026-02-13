99115670c60093a17d229e37ba94dd8db3d413b1
martin-velay <mvelay@lowrisc.org>
2026-02-13 15:26:51 +0100

    [util] Update Verilator runner
    - This change is required as the SW now performs special writes for the
      DV to know when the simulation should be terminated. In Verilator
      this was handled by a "magic word" write on the UART.
    
    Signed-off-by: martin-velay <mvelay@lowrisc.org>

--- util/verilator_runner.sh
+++ util/verilator_runner.sh
@@ -4,18 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 ROOT_DIR=$(dirname "$0")/..
-LOG_FILE="uart0.log"
 
 timeout 5m $ROOT_DIR/build/lowrisc_mocha_top_chip_verilator_0/sim-verilator/Vtop_chip_verilator \
   -E $1 > /dev/null 2>&1
-
-if grep -q "TEST RESULT: FAIL" "$LOG_FILE"; then
-  echo "Test failed"
-  exit 1
-elif grep -q "TEST RESULT: PASSED" "$LOG_FILE"; then
-  echo "Test passed"
-  exit 0
-fi
-
-echo "Simulation crashed"
-exit 2
